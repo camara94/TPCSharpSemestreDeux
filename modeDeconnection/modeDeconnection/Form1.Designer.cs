2abb1fa951d77f81cbd7fac0ac763dc67e228b3e
Camara Laby Damaro <ldamaro98@gmail.com>
2019-05-29 07:53:25 +0200

    Mode déconnecté encore

---
+++ modeDeconnection/modeDeconnection/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace modeDeconnection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.masterBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.detailsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.masterBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.detailsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(41, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 192);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Supprimer";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(133, 192);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(480, 12);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(779, 23);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(219, 139);
+            this.dataGridView3.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(645, 230);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "remplir";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1088, 490);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.masterBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.detailsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.BindingSource masterBindingSource;
+        private System.Windows.Forms.BindingSource detailsBindingSource;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ modeDeconnection/modeDeconnection/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+
+namespace modeDeconnection
+{
+    public partial class Form1 : Form
+    {
+        
+        private OleDbConnection cnx;
+        
+        private OleDbCommand cmd;
+
+        private OleDbDataAdapter dta;
+
+        private DataSet dts = new DataSet();
+
+        private String sql;
+
+        private DataTable dtt;
+
+        private DataRow dtr;
+
+        private String cnxstr;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cnxstr = "Provider=Microsoft.ACE.oledb.12.0.0;data source=gestionDocBD.accdb";
+            cnx = new OleDbConnection();
+            cnx.ConnectionString = cnxstr;
+            
+            sql ="select doc.*, proprietaire.* from doc inner join proprietaire on doc.propriétaire=proprietaire.id" ;
+            cmd = new OleDbCommand(sql);
+            dta = new OleDbDataAdapter(cmd);
+            cmd.Connection = cnx;
+            dta.Fill(dts);
+            dataGridView1.DataSource = dts;
+            dataGridView1.DataMember = dts.Tables[0].TableName;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.RemoveAt(dataGridView1.SelectedRows[0].Index);
+        }
+
+        private void master_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Specify a connection string. Replace the given value with a 
+                // valid connection string for a Northwind SQL Server sample
+                // database accessible to your system.
+                String connectionString = "Provider=Microsoft.ACE.oledb.12.0.0;data source=gestionDocBD.accdb";
+                OleDbConnection connection = new OleDbConnection(connectionString);
+
+                // Create a DataSet.
+                DataSet data = new DataSet();
+                data.Locale = System.Globalization.CultureInfo.InvariantCulture;
+
+                // Add data from the Customers table to the DataSet.
+                OleDbDataAdapter masterDataAdapter = new
+                    OleDbDataAdapter("select * from proprietaire", connection);
+                masterDataAdapter.Fill(data, "proprietaire");
+
+                // Add data from the Orders table to the DataSet.
+                OleDbDataAdapter detailsDataAdapter = new
+                    OleDbDataAdapter("select * from doc", connection);
+                detailsDataAdapter.Fill(data, "doc");
+
+                // Establish a relationship between the two tables.
+                DataRelation relation = new DataRelation("docproprietaire",
+                    data.Tables["proprietaire"].Columns["id"],
+                    data.Tables["doc"].Columns["Propriétaire"]);
+                data.Relations.Add(relation);
+
+                // Bind the master data connector to the Customers table.
+                masterBindingSource.DataSource = data;
+                masterBindingSource.DataMember = "proprietaire"; 
+
+                // Bind the details data connector to the master data connector,
+                // using the DataRelation name to filter the information in the 
+                // details table based on the current row in the master table. 
+                detailsBindingSource.DataSource = masterBindingSource;
+                detailsBindingSource.DataMember = "docproprietaire";
+
+                //liaison gridview*
+                dataGridView2.DataSource = masterBindingSource;
+                dataGridView3.DataSource = detailsBindingSource;
+            }
+            catch (SqlException)
+            {
+                MessageBox.Show("To run this example, replace the value of the " +
+                    "connectionString variable with a connection string that is " +
+                    "valid for your system.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dta.Update(dts, "doc");
+        }
+    }
+}
